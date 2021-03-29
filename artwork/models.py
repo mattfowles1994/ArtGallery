3aa8842527496c7137eec6995bcc12b0b538c4cb
Matthew Fowles <matafowles@googlemail.com>
2021-03-29 20:35:33 +0100

    Add search param model, amend artwork model to match

--- artwork/models.py
+++ artwork/models.py
@@ -6,8 +6,8 @@ class Artwork(models.Model):
         title = models.CharField(max_length=120)
         description = models.TextField(blank=True, null=True)
         price = models.DecimalField(decimal_places=2, max_digits=1000)
-        width = models.TextField()
-        height = models.TextField()
+        width = models.IntegerField()
+        height = models.IntegerField()
         artist = models.TextField(default='V.Galvao')
         available = models.BooleanField(null=True)
         images = models.ImageField(default='AG\images\default.PNG')
@@ -16,11 +16,23 @@ class Artwork(models.Model):
         def get_absolute_url(self):
                 return reverse("artwork:artwork", kwargs={"id": self.id})  #f"/artwork/{self.id}/"
 
+sortChoices = (
+    ('title','Title'),
+    ('price', 'Price'),
+    ('width','Width'),
+    ('height','Height'),
+    ('artist','Artist'),
+    ('year', 'Year'),
+)
+
 class SearchParams(models.Model):
+        title = models.CharField(max_length=120, blank=True, null=True)
         price = models.DecimalField(decimal_places=2, max_digits=1000)
-        width = models.TextField()
-        height = models.TextField()
-        artist = models.TextField()
+        width = models.IntegerField()
+        height = models.IntegerField()
+        artist = models.CharField(max_length=120)
         user = models.TextField()
+        year = models.IntegerField(blank=True, null=True)
+        sortBy = models.CharField(max_length=10, choices=sortChoices, default=title)
 
 
