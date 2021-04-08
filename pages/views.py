c220cf83cbe0b86e228d89a447a0090044453726
Matt Fowles <matafowles@googlemail.com>
2021-04-08 19:53:12 +0100

    Enact Filter functionality
    Filter function now works

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from artwork.models import Artwork, SearchParam
 from .forms import FilterForm
+from django.db.models import Q
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -10,21 +11,30 @@ def home_view(request, *args, **kwargs):
     searchset = SearchParam.objects.all().filter(user=request.user.id)
     #print(request.session)
     if 'search' in request.session:
-        try:
-            currentSearch = request.session['search']
-            currentSearch = SearchParam.objects.all().filter(id=currentSearch)
-            form = FilterForm(request.POST or None, initial={
-                "searchName": currentSearch[0].searchName,
-                "title": currentSearch[0].title,
-                "price": currentSearch[0].price,
-                "width": currentSearch[0].width,
-                "height": currentSearch[0].height,
-                "artist": currentSearch[0].artist,
-                "year": currentSearch[0].year
-                })
-        except:
-            form = FilterForm(request.POST or None)
-        
+        currentSearch = request.session['search']
+        currentSearch = SearchParam.objects.all().filter(id=currentSearch)
+        form = FilterForm(request.POST or None, initial={
+            "searchName": currentSearch[0].searchName,
+            "title": currentSearch[0].title,
+            "price": currentSearch[0].price,
+            "width": currentSearch[0].width,
+            "height": currentSearch[0].height,
+            "artist": currentSearch[0].artist,
+            "year": currentSearch[0].year
+            })
+
+        if currentSearch[0].title is not None:
+            queryset = queryset.filter(title__startswith=currentSearch[0].title)
+        if currentSearch[0].price is not None:
+            queryset = queryset.filter(price__lte=currentSearch[0].price)
+        if currentSearch[0].width is not None:
+            queryset = queryset.filter(width__lte=currentSearch[0].width)    
+        if currentSearch[0].height is not None:
+            queryset = queryset.filter(height__lte=currentSearch[0].height)
+        if currentSearch[0].artist is not None:
+            queryset = queryset.filter(artist__startswith=currentSearch[0].artist)
+        if currentSearch[0].year is not None:
+            queryset = queryset.filter(year=currentSearch[0].year)        
     else:
         form = FilterForm(request.POST or None)
 
