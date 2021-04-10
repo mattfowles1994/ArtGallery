c2d746021b833448db68bd50414a78c95c7b9450
Matt Fowles <matafowles@googlemail.com>
2021-04-10 11:42:13 +0100

    Add fucntion to clear search
    Fix issue with no search present

--- pages/views.py
+++ pages/views.py
@@ -12,32 +12,32 @@ def home_view(request, *args, **kwargs):
     #print(request.session)
     if 'search' in request.session:
         currentSearch = request.session['search']
+        print(currentSearch)
         currentSearch = SearchParam.objects.all().filter(id=currentSearch)
-        form = FilterForm(request.POST or None, initial={
-            "searchName": currentSearch[0].searchName,
-            "title": currentSearch[0].title,
-            "price": currentSearch[0].price,
-            "width": currentSearch[0].width,
-            "height": currentSearch[0].height,
-            "artist": currentSearch[0].artist,
-            "year": currentSearch[0].year
-            })
-
-        if currentSearch[0].title is not None:
-            queryset = queryset.filter(title__startswith=currentSearch[0].title)
-        if currentSearch[0].price is not None:
-            queryset = queryset.filter(price__lte=currentSearch[0].price)
-        if currentSearch[0].width is not None:
-            queryset = queryset.filter(width__lte=currentSearch[0].width)    
-        if currentSearch[0].height is not None:
-            queryset = queryset.filter(height__lte=currentSearch[0].height)
-        if currentSearch[0].artist is not None:
-            queryset = queryset.filter(artist__startswith=currentSearch[0].artist)
-        if currentSearch[0].year is not None:
-            queryset = queryset.filter(year=currentSearch[0].year)        
-    else:
-        form = FilterForm(request.POST or None)
-
+        try:
+            form = FilterForm(request.POST or None, initial={
+                "searchName": currentSearch[0].searchName,
+                "title": currentSearch[0].title,
+                "price": currentSearch[0].price,
+                "width": currentSearch[0].width,
+                "height": currentSearch[0].height,
+                "artist": currentSearch[0].artist,
+                "year": currentSearch[0].year
+                })
+            if currentSearch[0].title is not None:
+                queryset = queryset.filter(title__startswith=currentSearch[0].title)
+            if currentSearch[0].price is not None:
+                queryset = queryset.filter(price__lte=currentSearch[0].price)
+            if currentSearch[0].width is not None:
+                queryset = queryset.filter(width__lte=currentSearch[0].width)    
+            if currentSearch[0].height is not None:
+                queryset = queryset.filter(height__lte=currentSearch[0].height)
+            if currentSearch[0].artist is not None:
+                queryset = queryset.filter(artist__startswith=currentSearch[0].artist)
+            if currentSearch[0].year is not None:
+                queryset = queryset.filter(year=currentSearch[0].year)
+        except:
+            form = FilterForm(request.POST or None)
     if request.method == 'POST':
         if 'save' in request.POST:
             form = FilterForm(request.POST)
@@ -69,6 +69,11 @@ def home_view(request, *args, **kwargs):
             searchId = request.POST['old_search']
             request.session['search'] = searchId
             return redirect('home')
+        if 'reset' in request.POST:
+            form = FilterForm(request.POST)
+            if 'search' in request.session:
+                request.session['search'] = 0
+            queryset = Artwork.objects.all()
 
     my_context = {
         "object_list": queryset,
