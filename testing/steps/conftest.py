c97c5a278672dd312de01d5e24c433cd5443e241
Matt Fowles <matafowles@googlemail.com>
2021-04-19 20:26:54 +0100

    Add initial test for adding artwork

--- testing/steps/conftest.py
+++ testing/steps/conftest.py
@@ -1,10 +1,11 @@
 from django.core.management import call_command
 from selenium import webdriver
+from pytest_bdd import given, then, parsers
 import pytest
 
-@pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
-    pass
+#@pytest.fixture(autouse=True)
+#def enable_db_access_for_all_tests(db):
+#    pass
 
 @pytest.fixture(scope='session', autouse=True)
 def splinter_webdriver():
@@ -12,8 +13,22 @@ def splinter_webdriver():
     return 'chrome'
 
 
-#@pytest.fixture()
-#def django_db_setup(django_db_setup, django_db_blocker):
-#    # Load test db
-#    with django_db_blocker.unblock():
-#        call_command('loaddata', '../db.json')
+@pytest.fixture()
+def django_db_setup(django_db_setup, django_db_blocker):
+    # Load test db
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'testing/db.json')
+
+
+@given('an admin user is logged in')
+def admin_login(browser, client, live_server, db):
+    client.login(username='matt', password='goodison')
+    cookie = client.cookies['sessionid']
+    
+    browser.visit(live_server.url + '/admin/')
+    browser.cookies.add({'sessionid': cookie.value})
+    browser.reload()
+
+@then(parsers.parse('I see the "{title}" is in the list'))
+def verify_artwork_added(browser, title):
+    assert browser.links.find_by_text(title)
--- testing/steps/test_gallery.py
+++ testing/steps/test_gallery.py
@@ -2,27 +2,25 @@
 import os
 import pytest
 from pytest_bdd import scenarios, given, when, parsers, then
+import time
 
 scenarios('../features/gallery.feature')
 
 @when('I select add artwork')
 def select_add_listing(browser, live_server, client, admin_user):
-    client.login()
-    cookie = client.cookies['sessionid']
-    
-    browser.visit(live_server.url + '/admin/')
-    browser.cookies.add({'sessionid': cookie.value})
-    browser.reload()
-    #browser.links.find_by_partial_href(f'/artwork/add/').click()
 
-#@when(parsers.parse('I add a piece of artwork with the name "{title}"'))
-#def add_artwork(browser, live_server, title):
-#    browser.fill('title', title)
-#    browser.fill('price', 20
-#    browser.fill('width', 30)
-#    browser.fill('height', 40)
-#
-#@then('Then I see the artwork "{title}" was added successfully. message')
-#def step_impl(context):
-#    br = context.browser
-#    assert br.current_url.endswith('/admin/artwork/artwork/')
+    browser.links.find_by_partial_href(f'/artwork/add/').click()
+
+
+@when(parsers.parse('I add a piece of artwork with the name "{title}"'))
+def add_artwork(browser, live_server, title):
+    browser.fill('title', title)
+    browser.fill('price', 20)
+    browser.fill('width', 30)
+    browser.fill('height', 40)
+    browser.find_by_name(f'_save').click()
+    #time.sleep(20)
+
+#@then(parsers.parse('I see the "{title}" is in the list'))
+#def verify_artwork_added(browser, title):
+#    assert browser.links.find_by_text(title)
