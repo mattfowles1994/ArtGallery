b88a56b8fee9cdbdddc2b9039267aa35b46c76fd
Matt Fowles <matafowles@googlemail.com>
2021-04-21 21:25:22 +0100

    Issue #8
    Add minimum value to numerical fields of artwork

--- artwork/models.py
+++ artwork/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Artwork(models.Model):
         title = models.CharField(max_length=120, unique=True)
         description = models.TextField(blank=True, null=True)
-        price = models.DecimalField(decimal_places=2, max_digits=1000)
-        width = models.IntegerField()
-        height = models.IntegerField()
+        price = models.DecimalField(decimal_places=2, max_digits=1000, validators=[MinValueValidator(1)])
+        width = models.IntegerField(validators=[MinValueValidator(1)])
+        height = models.IntegerField(validators=[MinValueValidator(1)])
         artist = models.TextField(default='V.Galvao')
         available = models.BooleanField(default=0)
         images = models.ImageField(default='AG\images\default.PNG')
-        year = models.IntegerField(default='0')
+        year = models.IntegerField(default='0', validators=[MinValueValidator(1)])
 
         def get_absolute_url(self):
                 return reverse("artwork:artwork", kwargs={"id": self.id})  #f"/artwork/{self.id}/"
